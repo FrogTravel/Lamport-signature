42faa9677964e420ad8cda708ab9d69467990957
Ekaterina Levchenko <e.levchenko@innopolis.ru>
2021-09-15 15:00:02 +0300

    Comments

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import(
 
 const (
 	numberOfPairs = 256
+	byteLength = 8
 )
 
 func main(){
@@ -24,18 +25,15 @@ func main(){
 	fmt.Println(isVerified)
 }
 
-func printKey(key [numberOfPairs][2][]byte){
-	for i := 0; i < numberOfPairs; i++{
-		fmt.Println(key[i])		
-	}
-}
-
+// Method to generate a single block of 256 signature length. Creates a block of length 32 byte (256 bit) and fills it with random numbers
 func generateBlock() (block []byte) {
 	block = make([]byte, 32)
 	rand.Read(block)
 	return 
 }
 
+// There are two private keys, each is 256 lenght long, each block is 256 bits long (32 bytes). It means that in each block there will be 32 numbers in it. 
+// Public key is hashed private key. 
 func GenerateKey() (privateKey [numberOfPairs][2][]byte, publicKey [numberOfPairs][2][]byte) {
 	for i := 0; i < numberOfPairs; i++ {
 		for j := 0; j < 2; j++{
@@ -57,14 +55,20 @@ func hashBlock(keyToHash []byte) []byte{
 	return hashedKey[:]
 }
 
+// For signing the message we first get sha256 for the message 
+// Based on the hash we iterate through each bit of the hashedMessage 
+// If the bit is 0 we take block from first private key
+// If 1 -> from second 
+// Because go doesn't support flatMap we are iterating through the signed message again to flat the array.
+// NOTE: If you see more optimal solution please take a minute and message me as an Issue or to email: guinea.pig.prog@gmail.com
 func Sign(stringMessage string, privateKey [numberOfPairs][2][]byte) (signedMessage [256][]byte){
 	message := []byte(stringMessage)
 	hashedMessage := sha256.Sum256(message)
 
 	complexMessage := [32][8][]byte{}
 
-	for i := 0; i < 32; i++ { // длина сообщения 256 bit -> 32 byte. Я могу проходится только побайтово (по числам в массиве) Значит мне нужно пройтись 32 раза
-		complexMessage[i] = getSignForNumber(hashedMessage[i], privateKey[8 * i : 8 * (i + 1)]) // В private key нужно передать privateKey[0, 8]
+	for i := 0; i < 32; i++ { // the length if message is 256 bit -> 32 bytes. We can iterate only byte by byte (by numbers in array). It means I have to iterate 32 times
+		complexMessage[i] = getSignForNumber(hashedMessage[i], privateKey[8 * i : 8 * (i + 1)]) // В privateKey argument we have to pass privateKey 8 blocks
 	}
 
 	// flatting arrays 
@@ -78,18 +82,28 @@ func Sign(stringMessage string, privateKey [numberOfPairs][2][]byte) (signedMess
 	return
 }
 
+// Pass the byte (we cannot actually iterate through each bit of hashedMessage) 
+// Iterate with size of byte (which is always 8 and stated in constants) 
+// To get desired bit shift current byte to 7 - i; Then mask the result with 00000001 so you have only the smallest bit 
+// EXAMPLE: byte = 10101010
+// i = 0 -> number = 00000001 & 00000001 -> number = 00000001
+// i = 1 -> number = 00000010 & 00000001 -> number = 00000000
+// ...
+// i = 7 -> number = 10101010 & 00000001 -> number = 00000001
 func getSignForNumber(message byte, block [][2][]byte) (signedBlock [8][]byte) {
-	for i := 0; i < 8; i++ {
+	for i := 0; i < byteLength; i++ {
 		number := (message >> (7 - i)) & 1
 		signedBlock[i] = block[i][number]
 	}
 	return
 }
 
+// To verify the message (to verify that the person who claims it theirs message) we have to have the public key and the signed message. The latter is partial private key. 
+// What we have to do is to iterate throught each block in signed message, hash each block and to compare with public key. There is two blocks for each public key. 
+// We compare hashed message block with each block of public key. If they both are different then it is not verified. 
 func Verify(signedMessage [256][]byte, publicKey [numberOfPairs][2][]byte) (isVerified bool){
 	for i := 0; i < 256; i++ {
-		_hashedBlock := sha256.Sum256(signedMessage[i])
-		hashedBlock := _hashedBlock[:]
+		hashedBlock := sha256.Sum256(signedMessage[i])
 		for j := range hashedBlock {
 			if hashedBlock[j] != publicKey[i][0][j] && hashedBlock[j] != publicKey[i][1][j]{
 				return false
